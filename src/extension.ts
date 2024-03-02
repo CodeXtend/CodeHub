481b47763daa129ddf424deea9bc2cfc2e01f83a
ADITYAbasude <adityabasude13@gmail.com>
2024-03-02 19:47:58 +0530

    sidebar initial ui added

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,15 @@ import { SidebarProvider } from './Sidebar';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"CodeHub-sidebar",
+			sidebarProvider
+		)
+	);
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "CodeHub" is now active!');
